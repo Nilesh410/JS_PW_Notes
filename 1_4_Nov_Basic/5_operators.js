66991316d267f8d8c15b525af11d3c404497b4db
Nilesh_Shirude <shirudenileshs@gmail.com>
2023-11-04 07:14:44 +0000

    Study of Operators

---
+++ 1_4_Nov_Basic/5_operators.js
@@ -0,0 +1,26 @@
+//Arithmetic Operators
+/*
+5+5
+10-5
+5*5
+10/2
+*/
+const number_result1="2"+"4"
+console.log(number_result1)
+
+//2**3
+//10%3 =>reminder
+//
+let valueone=3
+valueone++   //shorthand notation
+
+//Assignment Operators
+let valuetwo=3
+valuetwo+=10
+console.log(valuetwo)
+
+//Comparison Operator
+let pw=3
+let pwskills=3
+console.log(pw===pwskills)
+console.log(pw!=pwskills)
