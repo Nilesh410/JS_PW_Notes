15c844350c64114133ba8ea46e1af5046b805802
Nilesh_Shirude <shirudenileshs@gmail.com>
2023-11-04 05:51:42 +0000

    Study about variables

---
+++ 1_4_Nov_Basic/3_variables.js
@@ -0,0 +1,9 @@
+//variable-let
+//constants-const
+
+let email="nsshirude@pict.edu"
+const user_name="Nilesh"
+//it does not start with number and special character
+
+user_name="Shirude"
+console.log(user_name)
