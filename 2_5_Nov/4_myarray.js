a57f4138113af1dcf5f03dec2654ec862a1e6f75
Nilesh_Shirude <shirudenileshs@gmail.com>
2023-11-05 06:19:40 +0000

    array to commit

---
+++ 2_5_Nov/4_myarray.js
@@ -0,0 +1,21 @@
+const myArray=[1,2,3,4,5]
+console.log(myArray)
+const myNewArray =new Array(1,2,3,4,5)
+console.log(myNewArray)
+
+const dcHeros=["batman","superman",true,NaN]
+console.log(dcHeros)
+console.log(dcHeros.length)
+console.log(dcHeros.push("Shaktiman"))
+console.log(dcHeros)
+console.log(dcHeros.pop())
+console.log(dcHeros.unshift("Nilesh"))
+console.log(dcHeros)
+console.log(dcHeros.toString())
+console.log(dcHeros.join("-"))
+console.log(dcHeros.slice(1,3))
+console.log(dcHeros)
+console.log(dcHeros.splice(1,2))
+console.log(dcHeros)
+
+
