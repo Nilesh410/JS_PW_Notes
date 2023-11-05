c32be8fb885695e8fb862b6b70a657f2b62ba395
Nilesh_Shirude <shirudenileshs@gmail.com>
2023-11-05 05:42:13 +0000

    add Number and math library

---
+++ 2_5_Nov/3_number.js
@@ -0,0 +1,10 @@
+const num=new Number(5.234567)
+console.log(num.toFixed(2))
+
+const subscribers=10000
+console.log(subscribers.toLocaleString())
+console.log(Number.MAX_VALUE)
+
+//Math Library
+console.log(Math.random()*10)
+console.log(Math.floor(Math.random()*6)+1)
