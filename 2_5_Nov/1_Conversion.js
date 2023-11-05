e3cd287e7188bcf5e31ff016c13ac6c7add0848e
Nilesh_Shirude <shirudenileshs@gmail.com>
2023-11-05 05:29:05 +0000

    string in js

---
+++ 2_5_Nov/1_Conversion.js
@@ -0,0 +1,15 @@
+let bankBalance='abc'
+
+let bankBalanceinInt=Number(bankBalance)
+
+console.log(bankBalance)
+console.log(bankBalanceinInt);
+console.log(typeof(bankBalanceinInt))
+
+console.log(NaN==NaN)
+console.log(NaN===NaN)
+
+console.log(5+undefined)
+console.log(5+null)
+
+console.log(undefined==null)
---
+++ 2_5_Nov/2_numandstring.js
@@ -0,0 +1,16 @@
+const firstName="Nilesh"
+const lastName="Shirude"
+
+console.log(firstName+''+lastName)
+console.log(`My firstaname is ${firstName} and latname is ${lastName}`)
+const userName=new String("Nilesh")
+console.log(userName[0])
+console.log(userName.length)
+console.log(userName.charAt(2))
+console.log(userName.replace('N','M'))
+console.log(userName)
+
+console.log(userName.includes("Nil"))
+console.log(userName.toUpperCase())
+userName.re
+
