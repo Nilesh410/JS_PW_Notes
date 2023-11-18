8a1d3e07c6b5fe3df01498087b64fa3fc1ff0b1c
Nilesh_Shirude <shirudenileshs@gmail.com>
2023-11-18 05:58:20 +0000

    Add if else ladder

---
+++ 3_18_Nov/2_If_elseladder.js
@@ -0,0 +1,29 @@
+let star="1"
+star=parseInt(star)
+
+if(star==1)
+{
+    console.log(`Service is poor because star is ${star}`)
+}
+else if(star==2)
+{
+    console.log(`Service is ok because star is ${star}`)
+}
+else if(star==3){
+    console.log(`Service is average because star is ${star}`)
+}
+else if(star==4)
+{
+    console.log(`Service is cool because star is ${star}`)
+}
+else if(star==5)
+{
+    console.log(`Service is good because star is ${star}`)
+}
+else if(star==6)
+{
+    console.log(`Service is Better because star is ${star}`)
+}
+else {
+    console.log(`Service is Awesome because star is ${star}`)
+}
