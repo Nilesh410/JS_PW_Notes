17d0fcb2473abc50b7ef3fcf9af6dad4337346a8
Nilesh_Shirude <shirudenileshs@gmail.com>
2023-11-19 06:09:47 +0000

    Add string method1

---
+++ 4_19_Nov/String_Method.js
@@ -0,0 +1,20 @@
+//String Methods from JAVA Scripts
+
+let username = "Nilesh Suresh Shirude"
+
+//1) length (it is property)
+
+console.log(username.length)
+
+console.log(username.slice(2,5))
+console.log(username.slice(2))
+console.log(username.slice(-5,-2))
+console.log(username.substring(2,9))
+console.log(username.substring(-5,-2))
+console.log(username.substr(2,6))
+console.log(username.substr(2))
+console.log(username.substr(-2))
+console.log(username.substr(-2,-1))
+
+
+
