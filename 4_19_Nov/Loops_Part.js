7e0405956a14a089457c693eaa9d7443165e9974
Nilesh_Shirude <shirudenileshs@gmail.com>
2023-11-19 06:44:07 +0000

    Add For Loop2

--- 4_19_Nov/Loops_Part.js
+++ 4_19_Nov/Loops_Part.js
@@ -1,5 +1,48 @@
-let my_array = ["Nilesh", "suresh", "Shirude"]
-for(let index=0;index<my_array.length;index++)
+//For Loop
+
+// let my_array = ["Nilesh", "suresh", "Shirude"]
+// for(let index=0;index<my_array.length;index++)
+// {
+//     console.log(my_array[index])
+// }
+
+let number=1
+// for(number;number<5;number++)
+// {
+//     if(number>3)
+//        break
+//     else
+//        console.log(number)
+// }
+
+// for(number;number<5;number++)
+// {
+//     if(number>3)
+//        break
+//     else
+//        console.log(number)
+// }
+
+// for(number;number<5;)
+// {
+//     if(number>3)
+//        break
+//     else
+//        console.log(number)
+// }
+
+// for(number;;number++)
+// {
+//     if(number>3)
+//        break
+//     else
+//        console.log(number)
+// }
+
+for(number;number<5;number++)
 {
-    console.log(my_array[index])
+    if(number>3)
+       continue
+    else
+       console.log(number)
 }
