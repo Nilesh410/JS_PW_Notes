3e0c86df8cf35deef4a9b92949ac738d876b317e
Nilesh_Shirude <shirudenileshs@gmail.com>
2023-11-25 05:30:04 +0000

    add suare function

--- 5_25_Nov/functionstudy.js
+++ 5_25_Nov/functionstudy.js
@@ -9,4 +9,18 @@
  */ 
 //Blcok:- collection of valid statements
 
+/* How to call the function 
+ nameOffuncction(parameter value)
+ */
+
 console.log("Study of Function")
+
+function calculateSquare(num)
+{
+    //logic for calculate the square of a number
+    let squareValue=num*num
+    return squareValue
+}
+
+console.log(calculateSquare(5))
+
