75dae8dd16892a7a2586080b338bfcc55ebe1355
Nilesh_Shirude <shirudenileshs@gmail.com>
2023-11-25 18:37:05 +0000

    add palindrome code

---
+++ class_homework/palindromeCheck.js
@@ -0,0 +1,17 @@
+let givenString="racecar"
+function palindromeCheck(str)
+{
+    let reverseString=""
+    for(let i=str.length-1;i>=0;i--)
+    {
+        reverseString+=str.charAt(i)
+    }
+    if(str===reverseString)
+     {
+         return "Given string is a palindrome"
+     }
+     else{
+        return "Given string is not a palindrome"
+     }
+}
+console.log(`${givenString} : `,palindromeCheck(`${givenString}`))
