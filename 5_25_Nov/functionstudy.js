c5ff7ce8416e029693f49b5a4dd752ad4e731ca3
Nilesh_Shirude <shirudenileshs@gmail.com>
2023-11-25 05:24:19 +0000

    add function file

---
+++ 5_25_Nov/functionstudy.js
@@ -0,0 +1,12 @@
+//Defintion
+//Theory
+//Use
+/* Syntax
+  function functionname(parameter list){
+    body
+    return output (not mandatory) automatically retrun undefined
+  }
+ */ 
+//Blcok:- collection of valid statements
+
+console.log("Study of Function")
