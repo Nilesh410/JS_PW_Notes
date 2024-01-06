9363c4d9381203e8091bf42b0c8f9471f2f952da
Nilesh410 <shirudenileshs@gmail.com>
2024-01-07 00:02:29 +0530

    add description about for each method

--- practice_23_24Dec/first.js
+++ practice_23_24Dec/first.js
@@ -1,6 +1,9 @@
+//for each :- it go to the every element of array and apply to the call back and execute 
+//the call back function. It does not return anything. 
 const arr=[2,4,8,3,5,7]
 console.log("Result of for each")
 const result=arr.forEach((ele)=>{
     console.log("array ele:",ele)
+    return ele
 })
 console.log(result)
