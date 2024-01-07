7a2c37479dd7f33109b677401dfb2c00708f20e7
Nilesh410 <shirudenileshs@gmail.com>
2024-01-07 11:43:03 +0530

    study synac and async workding of JS

---
+++ 10_7_8_Jan24/sync_async.js
@@ -0,0 +1,28 @@
+for(let i=0;i<100000000;i++)
+{  
+}
+console.log("end of first for loop")  
+for(let i=0;i<1000000000;i++)
+{
+  
+}
+console.log("end of second for loop")
+console.log("Start Timer 1")
+setTimeout(()=>{
+    console.log("End of timer 1")
+},20000)
+console.log("Start Timer 2")
+setTimeout(()=>{
+    console.log("End of timer 2")
+},0)
+
+for(let i=0;i<1000;i++)
+{
+    
+}
+console.log("end of 3rd for loop")
+
+console.log("Start Timer 3")
+setTimeout(()=>{
+    console.log("End of timer 3")
+},100)
