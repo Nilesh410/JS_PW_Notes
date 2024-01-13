1bd261e18798d7e808e075fac22e7539143bb55e
Nilesh410 <shirudenileshs@gmail.com>
2024-01-13 11:03:51 +0530

    add old file

--- 10_7_8_Jan24/sync_async.js
+++ 10_7_8_Jan24/sync_async.js
@@ -8,14 +8,16 @@ for(let i=0;i<1000000000;i++)
 }
 console.log("end of second for loop")
 console.log("Start Timer 1")
-setTimeout(()=>{
+setTimeout(()=>{      //JS send the request to runtime envrioment for no native function and immediatly comback
     console.log("End of timer 1")
-},20000)
+},20000)   //after timer complete RE send call back function to event queue
 console.log("Start Timer 2")
-setTimeout(()=>{
+setTimeout(()=>{    //JS send the request to runtime envrioment for no native function and immediatly comback
     console.log("End of timer 2")
-},0)
-
+},0) //after timer complete RE send call back function to event queue
+//event loop - keep constiuously run itself
+// event loop - check always that any thing is execute on main thread
+//if main thread is empth then send call back function from event queue to call stack 
 for(let i=0;i<1000;i++)
 {
     
@@ -23,6 +25,6 @@ for(let i=0;i<1000;i++)
 console.log("end of 3rd for loop")
 
 console.log("Start Timer 3")
-setTimeout(()=>{
+setTimeout(()=>{    //JS send the request to runtime envrioment for no native function and immediatly comback
     console.log("End of timer 3")
-},100)
+},100) //after timer complete RE send call back function to event queue
