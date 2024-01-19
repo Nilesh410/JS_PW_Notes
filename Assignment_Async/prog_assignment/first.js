c82f775ccc2f17037ad15bf362ceace1748c523a
Nilesh410 <shirudenileshs@gmail.com>
2024-01-19 16:48:05 +0530

    add progamming assignment solution

---
+++ Assignment_Async/prog_assignment/first.js
@@ -0,0 +1,37 @@
+let ur="This is url"
+let data="this is data"
+let new_url="this is new url"
+let file="this is file name"
+
+let cb1=()=>{
+    console.log("from downloader callback function")
+     }
+let cb2=()=>{
+        console.log("from filewriting callback function")
+         }
+ let cb3=()=>{
+            console.log("from uploading callback function")
+             }
+     
+function downloader(ur,cb)
+{
+    // write a dummy impl using setTimeout to show a delay 
+    setTimeout(cb,4000);
+    console.log(ur);
+}
+
+function writeFile(data, cb) {
+    // write a dummy impl using setTimeout to show a delay 
+    setTimeout(cb,2000);
+    console.log(data);
+}
+function uploadFile(fileName, newUrl, cb) {
+     // write a dummy impl using setTimeout to show a delay 
+     setTimeout(cb,3000);
+     console.log(fileName);
+    console.log(newUrl);
+}
+
+downloader(ur,cb1)
+writeFile(data,cb2)
+uploadFile(file,new_url,cb3)
